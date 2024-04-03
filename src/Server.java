9392bfc96977c57ad06ec3a438f9f57716d763a4
Jitendra <jitendrastark8853@gmail.com>
2024-04-03 14:48:42 +0530

    code

---
+++ src/Server.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class Server extends JFrame {
+    Server(){
+
+
+
+
+
+
+
+
+
+
+        setSize(400,700);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        new Server();
+
+    }
+}
